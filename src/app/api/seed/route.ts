6cb36d8fbbc8f1348260be816fde67625f48bce3
AlexisFlores17 <alex.p.flores17@gmail.com>
2024-01-20 15:16:38 -0600

    setup data base and prisma

---
+++ src/app/api/seed/route.ts
@@ -0,0 +1,23 @@
+import prisma from '@/lib/prisma'
+import { NextResponse, NextRequest } from 'next/server'
+
+export async function GET(request: Request) { 
+
+    //purgar base de datos
+     await prisma.todo.deleteMany();
+
+    //Crear seed
+
+    await prisma.todo.createMany({
+        data:[
+            { description:"Piedra del alma", complete:true},
+            { description:"Piedra del poder"},
+            { description:"Piedra del tiempo"},
+            { description:"Piedra del dragon"},
+            { description:"Piedra del sol"}
+        ]
+    })
+  return NextResponse.json({
+    message:'Seed executed'
+  })
+}
