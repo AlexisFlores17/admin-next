ae063ec423b27626c3acfb14b0bf8f177f810858
AlexisFlores17 <alex.p.flores17@gmail.com>
2024-01-24 16:54:28 -0600

    24 enero

--- src/todos/components/TodosGrid.tsx
+++ src/todos/components/TodosGrid.tsx
@@ -5,6 +5,7 @@ import { TodoItem } from ".."
 
 import * as todosApi from "@/todos/helpers/todos";
 import { useRouter } from "next/navigation";
+import { toggleTodo } from "../actions/todo-actions";
 
 interface Props {
     todos?: Todo[]
@@ -14,11 +15,11 @@ export const TodosGrid = ({todos = []}:Props) => {
 
     const router = useRouter();
 
-    const toggleTodo = async(id:string, complete:boolean) =>{
+    // const toggleTodo = async(id:string, complete:boolean) =>{
 
-        const updatedTodo = await todosApi.updateTodo(id,complete)
-        router.refresh();
-    }
+    //     const updatedTodo = await todosApi.updateTodo(id,complete)
+    //     router.refresh();
+    // }
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
